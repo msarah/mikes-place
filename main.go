2bf95c2dae48a2adf8f6bf0132850fbabb68e839
msarah <murphy.sarahlf@gmail.com>
2017-10-04 11:40:19 -0400

    index handler

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 )
 
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*"))
+}
+
 func main() {
 
-	http.StripPrefix("/assets/", http.FileServer(http.Dir()))
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+
+	http.HandleFunc("/", index)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	if err := tpl.ExecuteTemplate(w, "index.html", nil); err != nil {
+		log.Fatalln(err)
+	}
 }
